207447b7064669b240f75359868e1bb8f3b84e7b
Yogesh Singh <yogeshsinghs607@gmail.com>
2024-04-20 22:14:33 +0530

    add internship tab

--- src/components/sectionHeading/sectionHeading.tsx
+++ src/components/sectionHeading/sectionHeading.tsx
@@ -3,10 +3,11 @@ import './sectionHeading.css';
 
 type HeadingProps = {
     title: string;
+    headingSize?: string;
 }
 
 export default function SectionHeading(props: HeadingProps) {
     return (
-        <h3 className="text-2xl pb-8 pl-14 pr-14 lg:pr-7 font-bold relative heading-dash">{props.title}</h3>
+        <h3 className={(props.headingSize ? props.headingSize : "text-2xl") + " pb-8 pl-14 pr-14 lg:pr-7 font-bold relative heading-dash"}>{props.title}</h3>
     );
 }
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/home';
 import Gallery from '../pages/gallery/gallery';
+import Internship from '../pages/internship/internship';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
           <Route key="home" path='/' element={<Home />} />
           <Route key="home-named" path='/maa-the-foundation' element={<Home />} />
           <Route key="gallery" path='/gallery' element={<Gallery />} />
+          <Route key="internship" path='/internship' element={<Internship />} />
         </Routes>
       </BrowserRouter>
     </div>
